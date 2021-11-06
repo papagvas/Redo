30110d010580615d820ae34923d5c25ee5d78dcf
papagvas <papagvasexy@gmail.com>
2021-11-06 15:07:00 +0300

    Almost redo-ifchange implemented

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Redo
 redo is a build system by D. J. Bernstein
+
+## Usage
+
+redo *target*
--- redo.hs
+++ redo.hs
@@ -1,21 +1,33 @@
-import System.Directory (renameFile, removeFile)
-import System.Environment (getArgs)
+import Control.Monad (filterM)
+import Data.Map.Lazy (insert, fromList, toList, adjust)
+import Data.Maybe (listToMaybe)
+import System.Directory (renameFile, removeFile, doesFileExist)
+import System.Environment (getArgs, getEnvironment)
 import System.Exit (ExitCode(..))
+import System.FilePath (hasExtension, replaceBaseName, takeBaseName)
 import System.IO (hPutStrLn, stderr)
-import System.Process (createProcess, waitForProcess, shell)
+import System.Process (createProcess, waitForProcess, shell, CreateProcess(..))
 
 main :: IO ()
-main = do
-  args <- getArgs
-  mapM_ redo args
+main = getArgs >>= mapM_ redo
   
 
 redo :: String -> IO ()
 redo target = do
-  let tmp = target ++ "---redoing"
-  (_, _, _, ph) <- createProcess $ shell $ "sh " ++ target ++ ".do - - " ++ tmp ++ " > " ++ tmp
-  exit <- waitForProcess ph
-  case exit of
-    ExitSuccess -> do renameFile tmp target
-    ExitFailure code -> hPutStrLn stderr ("Redo script exited with a non-zero exit code: " ++ show code) >> removeFile tmp
-  return ()
+  redoPath target >>= maybe (error $ "No .do file found for target '" ++ target ++ "'") redo'  
+  where redo' :: FilePath -> IO ()
+        redo' path = do
+          oldEnv <- getEnvironment
+	  let newEnv = toList $ adjust (++ ":.") "PATH" $ insert "REDO_TARGET" target $ fromList oldEnv
+          (_, _, _, ph) <- createProcess $ (shell $ cmd path) {env = Just newEnv}
+          exit <- waitForProcess ph
+          case exit of
+            ExitSuccess -> do renameFile tmp target
+            ExitFailure code -> hPutStrLn stderr ("Redo script exited with a non-zero exit code: " ++ show code) >> removeFile tmp
+        tmp = target ++ "---redoing"
+        cmd path = unwords ["sh", path, "0", takeBaseName target, tmp, ">", tmp]
+
+redoPath :: FilePath -> IO (Maybe FilePath)
+redoPath target = filterM doesFileExist candidates >>= return . listToMaybe
+  where candidates = [target ++ ".do"] ++ if hasExtension target then [replaceBaseName target "default" ++ ".do"] else []
+
