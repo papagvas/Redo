8a30688dfa137dd64bbb06e83b3d21163b1a6344
papagvas <papagvasexy@gmail.com>
2021-11-07 15:09:43 +0300

    Atomic rebuild almost done

--- redo.hs
+++ redo.hs
@@ -1,7 +1,7 @@
 import Control.Monad (filterM)
 import Data.Map.Lazy (insert, fromList, toList, adjust)
 import Data.Maybe (listToMaybe)
-import System.Directory (renameFile, removeFile, doesFileExist)
+import System.Directory (renameFile, removeFile, doesFileExist, getDirectoryContents)
 import System.Environment (getArgs, getEnvironment)
 import System.Exit (ExitCode(..))
 import System.FilePath (hasExtension, replaceBaseName, takeBaseName)
@@ -31,3 +31,15 @@ redoPath :: FilePath -> IO (Maybe FilePath)
 redoPath target = filterM doesFileExist candidates >>= return . listToMaybe
   where candidates = [target ++ ".do"] ++ if hasExtension target then [replaceBaseName target "default" ++ ".do"] else []
 
+upToDate :: String -> IO Bool
+upToDate target = do
+  sumsAndDeps <- readFile (depDir </> checksums) >>= return . words
+  mapM depUpToDate deps
+  where sums = [sumsAndDeps !! x | x <- [1,3..(length sumsAndDeps - 1)]]
+        deps = [sumsAndDeps !! x | x <- [0,2..(length sumsAndDeps - 1)]]
+	checksumDict = fromList $ zip deps sums  
+	depUpToDate :: [Char] -> IO Bool 
+	depUpTodate dep = lookup dep checksumDict >>= maybe (return False) (return . (md5hash dep ==))
+	
+	   
+	  
